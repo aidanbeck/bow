4d9a886a5f26b85e03f766542104671d7e4fe1fc
aidanbeck <aidanmbeck@outlook.com>
2025-10-02 14:10:13 -0500

    wheel changes color on movement

--- rotary.js
+++ rotary.js
@@ -1,25 +1,49 @@
-let rotaryUI = document.getElementById("rotaryUI");
-let movements = document.getElementById("movements");
-
 let touchOn = false;
 
-rotaryUI.ontouchstart = () => {
-    !touchOn && (rotaryUI.style.backgroundColor = "lightblue");
+let rotaryUI = document.getElementById("rotaryUI");
+
+let touch = {
+    start:    { x: 0, y: 0 },
+    position: { x: 0, y: 0 } // current position
 }
 
-rotaryUI.ontouchend = () => {
-    rotaryUI.style.backgroundColor = "black";
-}
-
-rotaryUI.ontouchmove = () => {
-    movements.innerHTML++;
-}
-
-
+/*
+    Toggle `touchOn` when anywhere on document is touched.
+    This allows us to check if a touch has already started.
+*/
 document.ontouchstart = () => {
     touchOn = true;
 }
-
 document.ontouchend = () => {
     touchOn = false;
+}
+
+// Touching within the rotary
+rotaryUI.ontouchstart = (event) => {
+    if (!touchOn) {
+        rotaryUI.style.backgroundColor = "lightblue";
+        touch.start.x = event.touches[0].clientX;
+        touch.start.y = event.touches[0].clientY;
+    }
+}
+rotaryUI.ontouchend = () => {
+    rotaryUI.style.backgroundColor = "black";
+}
+rotaryUI.ontouchmove = (event) => {
+    
+    touch.position.x = event.touches[0].clientX;
+    touch.position.y = event.touches[0].clientY;
+
+    let touchWidth = touch.position.x - touch.start.x;
+    let touchHeight = touch.position.y - touch.start.y;
+    let touchDifference = touch.position.x - touch.position.y;
+
+    let middle = 128; //middle of rgb spectrum
+
+    touchWidth += middle;
+    touchHeight += middle;
+    touchDifference += middle;
+
+    rotaryUI.style.backgroundColor = `rgb(${touchWidth}, ${touchHeight}, ${touchDifference})`;
+    
 }
