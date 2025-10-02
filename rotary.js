0e91e03178a8a8e8ce833ccaa841d15e4f1810ee
aidanbeck <aidanmbeck@outlook.com>
2025-10-02 15:23:01 -0500

    markers & origin

--- rotary.js
+++ rotary.js
@@ -1,27 +1,35 @@
-let touchOn = false;
-
+let markers = document.getElementsByClassName("marker");
 let rotaryUI = document.getElementById("rotaryUI");
-var rotaryCoordinates = rotaryUI.getBoundingClientRect();
 
 let touch = {
+    on: false,
     start:    { x: 0, y: 0 },
     position: { x: 0, y: 0 } // current position
 }
 
+var rotaryCoordinates = rotaryUI.getBoundingClientRect();
+let origin = {
+    x: rotaryCoordinates.x + (rotaryCoordinates.width / 2),
+    y: rotaryCoordinates.y + (rotaryCoordinates.height / 2)
+}
+markers[2].style.left = origin.x + "px";
+markers[2].style.top = origin.y + "px";
+
 /*
-    Toggle `touchOn` when anywhere on document is touched.
+    Toggle `touch.on` when anywhere on document is touched.
     This allows us to check if a touch has already started.
 */
 document.ontouchstart = () => {
-    touchOn = true;
+    touch.on = true;
 }
 document.ontouchend = () => {
-    touchOn = false;
+    touch.on = false;
 }
+
 
 // Touching within the rotary
 rotaryUI.ontouchstart = (event) => {
-    if (!touchOn) {
+    if (!touch.on) {
         setTouchStart(event);
     }
 }
@@ -38,12 +46,18 @@ rotaryUI.ontouchmove = (event) => {
     let touchDifference = touch.position.x - touch.position.y;
 
     setRotaryBackgroundColor(touchWidth, touchHeight, touchDifference);
+
+    markers[1].style.left = touch.position.x + "px";
+    markers[1].style.top = touch.position.y + "px";
 }
 
 function setTouchStart(event) {
     firstTouch = event.touches[0];
     touch.start.x = firstTouch.clientX;
     touch.start.y = firstTouch.clientY;
+
+    markers[0].style.left = touch.start.x + "px";
+    markers[0].style.top = touch.start.y + "px";
 }
 
 function setRotaryBackgroundColor(r, g, b) {
